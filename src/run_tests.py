ce30de17fb30d169d0ac1cdca4a74d257206a96a
FLEB <voterudy@voterudy.org>
2022-08-16 00:19:58 -0400

    Add builder script. Also fix the demo blendfile again, and put a main check on the run_scripts.py script.

--- src/run_tests.py
+++ src/run_tests.py
@@ -2,6 +2,12 @@ import bpy
 import unittest
 from os import path
 
-loader = unittest.TestLoader()
-all_tests = unittest.TestLoader().discover(path.dirname(__file__), "test_*.py", path.dirname(__file__))
-unittest.TextTestRunner(verbosity=2).run(all_tests)
+
+def run_tests():
+    loader = unittest.TestLoader()
+    all_tests = unittest.TestLoader().discover(path.dirname(__file__), "test_*.py", path.dirname(__file__))
+    unittest.TextTestRunner(verbosity=2).run(all_tests)
+
+
+if __name__ == "__main__":
+    run_tests()
