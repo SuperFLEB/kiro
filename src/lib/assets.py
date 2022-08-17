2015d4e79f1d867f97ae1e983665d024fd69b4a6
FLEB <voterudy@voterudy.org>
2022-08-17 01:40:58 -0400

    Mark the keysets used by the keycaps and sort them at the top: - Update the shader node to include the name - Read the name from the shader nodes on the selected item when cloning - Show an icon and sort the list with used keysets first
    This feature does only work if you remake the nodesets, as they've changed.

--- src/lib/assets.py
+++ src/lib/assets.py
@@ -1,21 +1,54 @@
-import bpy
-from os.path import exists, dirname, basename, join
+import json
 
-_LIBRARY_PATH = join(dirname(__file__), "..", "KiroImportables.blend")
+import bpy
+from os.path import dirname, abspath, join
+
+_LIBRARY_PATH = abspath(join(dirname(__file__), "..", "KiroImportables.blend"))
+_info = None
+
+
+def _unlink_library():
+    assets_lib = next((lib for lib in bpy.data.libraries if abspath(lib.filepath) == _LIBRARY_PATH), None)
+    if assets_lib:
+        pass
+        bpy.data.libraries.remove(assets_lib)
+    else:
+        print(f"Kiro: Could not find the library {_LIBRARY_PATH}")
 
 
 def load_grid_picker() -> bpy.types.ShaderNodeTree:
-    existing = [g for g in bpy.data.node_groups if g.name == "Kiro Grid Picker" and g["kiro_id"] == "GRID_PICKER"]
+    global _info
+
+    library_already_loaded = bool(next((lib for lib in bpy.data.libraries if lib.filepath == _LIBRARY_PATH), False))
+    imported = None
+
+    if not _info:
+        with bpy.data.libraries.load(_LIBRARY_PATH, link=False) as (src, imported):
+            imported.node_groups = ["Kiro Grid Picker"]
+            imported.texts = ["info.json"]
+
+        _info = json.loads(imported.texts[0].as_string())
+        bpy.data.texts.remove(imported.texts[0])
+
+    # Extract
+    major_minor = ".".join([str(mm) for mm in _info["version"][:-1]])
+    kiro_id = f"GRID_PICKER_{major_minor}"
+    existing: bpy.types.ShaderNodeTree = next((g for g in bpy.data.node_groups if g.name.startswith("Kiro Grid Picker") and "kiro_id" in g and g["kiro_id"] == kiro_id), None)
+
     if existing:
-        gp_nodegroup = existing[0]
-        print(f"Grid Picker already imported ({len(existing)} found). Using ", gp_nodegroup)
-        return gp_nodegroup
+        print(f"Grid Picker already imported ({existing.name}) found). Using ", existing)
+        return existing
+
     with bpy.data.libraries.load(_LIBRARY_PATH, link=False) as (src, imported):
         imported.node_groups = ["Kiro Grid Picker"]
 
+    if not library_already_loaded:
+        _unlink_library()
+
     # Done loading. Now "imported" contains the results in a list
     gp_nodegroup = imported.node_groups[0]
-    gp_nodegroup['kiro_id'] = "GRID_PICKER"
+    gp_nodegroup["kiro_id"] = kiro_id
     gp_nodegroup.asset_clear()
+
     return gp_nodegroup
 
--- src/lib/util.py
+++ src/lib/util.py
@@ -1,3 +1,4 @@
+import bpy
 import re
 
 
@@ -17,3 +18,26 @@ def wordwrap(string: str, length: int) -> list[str]:
             lines[-1] += f"{word} "
     lines = [line[:-1] for line in lines]
     return lines
+
+
+def get_collection_of_object(obj: bpy.types.Object, default_to_context: bool = True) -> bpy.types.Collection | None:
+    """Get the enclosing collection of an Object (caveat: if the object is not in the active scene, this may break)"""
+
+    # If there's only one, return that...
+    candidates = [c for c in obj.users_collection]
+
+    if len(candidates) == 0:
+        return bpy.context.scene.collection if default_to_context else None
+
+    if len(candidates) == 1:
+        return candidates[0]
+
+    # If there are more than one, but one isn't in the current Scene
+    # (e.g., it's a RigidBodyWorld), return the first that isn't...
+    scene_collections = [bpy.context.scene.collection] + list(bpy.context.scene.collection.children_recursive)
+    candidates = [c for c in candidates if c in scene_collections]
+    if len(candidates) > 0:
+        return candidates[0]
+
+    # Return the first collection from anywhere
+    return obj.users_collection[0]
