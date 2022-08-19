0cecbb5ac356a5e9c0f7237217b6ca1af0e463d5
FLEB <voterudy@voterudy.org>
2022-08-19 00:20:19 -0400

    Fix support for Collection Instances, namely dimensioning them properly using the bounding-boxer lib.
    Wipe the string when creating a new "from string" clone.
    
    Make the wrong-keyset detector less hair-trigger.

---
+++ src/lib/boxer.py
@@ -0,0 +1,48 @@
+from typing import Iterable
+import bpy
+from mathutils import Vector
+from . import util
+
+
+# See https://github.com/SuperFLEB/bounding_boxer for the full addon this lib is based off of
+
+def _get_extremes_vectors(vectors: Iterable[Vector]) -> tuple[Vector, Vector]:
+    min = [float("inf"), float("inf"), float("inf")]
+    max = [-float("inf"), -float("inf"), -float("inf")]
+    for vec in vectors:
+        for axis in range(3):
+            min[axis] = vec[axis] if vec[axis] < min[axis] else min[axis]
+            max[axis] = vec[axis] if vec[axis] > max[axis] else max[axis]
+    return Vector(min), Vector(max)
+
+
+def _get_points(obj: bpy.types.Object, mesh_precision: bool = False) -> list[Vector]:
+    data_type = type(obj.data)
+    if mesh_precision and type(obj.data) is bpy.types.Mesh:
+        return [v.co for v in obj.data.vertices]
+    else:
+        return [Vector(corner) for corner in obj.bound_box]
+
+
+def get_extremes(obj: bpy.types.Object, mesh_precision: bool = False, ttl: int = 20) -> tuple[Vector, Vector]:
+    """
+    Find the extremes of the object, deeply incorporating instanced collections
+    Note that this creates an imperfect bounding box in cases where rotation would change the world-axis
+    bounding box (e.g., a rounded cube), as the untranslated bounding box is translated to world coords.
+    """
+    if ttl == 0:
+        raise Exception("Parenting hierarchy is too deep")
+
+    # TODO: Other sorts of parenting/instancing, too?
+    if obj.instance_collection is None:
+        # Regular object
+        points = _get_points(obj, mesh_precision=mesh_precision)
+        return _get_extremes_vectors([obj.matrix_world @ Vector(pt) for pt in points])
+
+    extremes = []
+    # Collection Instance
+    for coll_obj in obj.instance_collection.objects:
+        coll_extremes = get_extremes(coll_obj, mesh_precision=mesh_precision, ttl=ttl - 1)
+        coll_extremes = [obj.matrix_local @ Vector(pt) for pt in coll_extremes]
+        extremes.append(coll_extremes)
+    return _get_extremes_vectors(util.flatten(extremes))
